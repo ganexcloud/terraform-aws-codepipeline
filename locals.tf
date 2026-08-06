0fa8d192da7827802342bb639ee4096741a0a6f5
caiohasouza <caio@ganex.com.br>
2026-08-05 23:05:49 -0300

    chore(terraform): standardize module repository

--- locals.tf
+++ locals.tf
@@ -1,6 +1,5 @@
 locals {
   role_arn       = var.role_arn == "" ? aws_iam_role.pipeline[0].arn : var.role_arn
   role_name      = var.role_name == "" ? "AWSCodePipelineServiceRole-${data.aws_region.current.name}-${var.name}" : var.role_name
-  webhook_secret = join("", random_string.webhook_secret.*.result)
-  webhook_url    = join("", aws_codepipeline_webhook.this.*.url)
+  webhook_secret = random_string.webhook_secret[0].result
 }
